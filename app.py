2702f41b42ddd8ae15f5ae84b499a5d1fa877b01
Manuel Octavio Lucio Magaña <mlucio@pagatodo.com>
2024-07-04 15:23:25 -0600

    chance number and version

--- app.py
+++ app.py
@@ -375,7 +375,7 @@ def enviar_mensajes_whatsapp(texto,number):
     connection = http.client.HTTPSConnection("graph.facebook.com")
 
     try:
-        connection.request("POST","/v18.0/117721278011867/messages", data, headers)
+        connection.request("POST","/v19.0/351317348068042/messages", data, headers)
         response = connection.getresponse()
         print(response.status, response.reason)
     except Exception as e:
